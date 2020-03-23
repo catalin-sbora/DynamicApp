b3a0b22a3e8a9168888f7814ef8957cd933dd8ea
catalin-sbora <catalin.sbora@gmail.com>
2020-03-23 06:36:08 -0700

    Create initial project

---
+++ DynamicApp/DynamicApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DynamicApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
